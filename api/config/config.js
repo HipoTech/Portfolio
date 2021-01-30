f53c607f5960c2c172c120cffbdc48e2f5b63288
HipoTech <p_dimitrov@abv.bg>
2021-01-30 10:48:32 +0200

    api now liten on port 80

--- api/config/config.js
+++ api/config/config.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = {
     development: {
-        port: process.env.PORT || 3100,
+        port: process.env.PORT || 80,
         dbURL: 'mongodb://localhost:27017/',
         cookie: 'ecom-auth-token'
     },
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    Test
+    Test2
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
