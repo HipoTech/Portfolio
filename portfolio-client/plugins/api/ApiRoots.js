a4d9aab66ef34d84f5a94579e4db249f4a9ca1b6
HipoTech <p_dimitrov@abv.bg>
2021-02-14 16:03:33 +0200

    testing localehost call

--- portfolio-client/plugins/api/ApiRoots.js
+++ portfolio-client/plugins/api/ApiRoots.js
@@ -1,31 +1,32 @@
+const main = 'http://localhost:3100';
+
 export default {
-  main: 'https://portfolioapi.plamen-dimitrov.eu//api',
 
   // User
-  user: 'https://portfolioapi.plamen-dimitrov.eu//api/user',
-  userRegister: `https://portfolioapi.plamen-dimitrov.eu//api/user/register`,
-  userLogIn: 'https://portfolioapi.plamen-dimitrov.eu//api/user/login',
-  userLogOut: 'https://portfolioapi.plamen-dimitrov.eu//api/user/logout',
-  userAuthenticate: 'https://portfolioapi.plamen-dimitrov.eu//api/user/auth',
+  user: `${main}/api/user`,
+  userRegister: `${main}/api/user/register`,
+  userLogIn: `${main}/api/user/login`,
+  userLogOut: `${main}/api/user/logout`,
+  userAuthenticate: `${main}/api/user/auth`,
 
   //Product
-  productCreate: 'https://portfolioapi.plamen-dimitrov.eu//api/products/create',
-  productGetAll: 'https://portfolioapi.plamen-dimitrov.eu//api/products/getAll',
-  productGetOne: 'https://portfolioapi.plamen-dimitrov.eu//api/products/getOne',
-  productEditOne: 'https://portfolioapi.plamen-dimitrov.eu//api/products/edit',
-  productDeleteOne: 'https://portfolioapi.plamen-dimitrov.eu//api/products/delete',
+  productCreate: `${main}/api/products/create`,
+  productGetAll: `${main}/api/products/getAll`,
+  productGetOne: `${main}/api/products/getOne`,
+  productEditOne: `${main}/api/products/edit`,
+  productDeleteOne: `${main}/api/products/delete`,
 
   //Category
-  categoryCreate: 'https://portfolioapi.plamen-dimitrov.eu//api/categories/create',
-  categoryGetAll: 'https://portfolioapi.plamen-dimitrov.eu//api/categories/getAll',
-  categoryGetOne: 'https://portfolioapi.plamen-dimitrov.eu//api/categories/getOne',
-  categoryEditOne: 'https://portfolioapi.plamen-dimitrov.eu//api/categories/edit',
-  categoryDeleteOne: 'https://portfolioapi.plamen-dimitrov.eu//api/categories/delete',
+  categoryCreate: `${main}/api/categories/create`,
+  categoryGetAll: `${main}/api/categories/getAll`,
+  categoryGetOne: `${main}/api/categories/getOne`,
+  categoryEditOne: `${main}/api/categories/edit`,
+  categoryDeleteOne: `${main}/api/categories/delete`,
 
   //Brand
-  brandCreate: 'https://portfolioapi.plamen-dimitrov.eu//api/brands/create',
-  brandGetAll: 'https://portfolioapi.plamen-dimitrov.eu//api/brands/getAll',
-  brandGetOne: 'https://portfolioapi.plamen-dimitrov.eu//api/brands/getOne',
-  brandEditOne: 'https://portfolioapi.plamen-dimitrov.eu//api/brands/edit',
-  brandDeleteOne: 'https://portfolioapi.plamen-dimitrov.eu//api/brands/delete',
+  brandCreate: `${main}/api/brands/create`,
+  brandGetAll: `${main}/api/brands/getAll`,
+  brandGetOne: `${main}/api/brands/getOne`,
+  brandEditOne: `${main}/api/brands/edit`,
+  brandDeleteOne: `${main}/api/brands/delete`,
 } 
