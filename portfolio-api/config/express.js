09cfad59116a8a6af65a2beebfa73d926239273e
HipoTech <p_dimitrov@abv.bg>
2021-02-14 16:12:48 +0200

    core policy updated with multiple domains

--- portfolio-api/config/express.js
+++ portfolio-api/config/express.js
@@ -21,7 +21,13 @@ module.exports = (app) => {
     })
 
     app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+        const allowedDomains = ['http://localhost:3000', 'https://plamen-dimitrov.eu'];
+        const origin = req.headers.origin;
+        if (allowedDomains.indexOf(origin) > -1) {
+            res.setHeader('Access-Control-Allow-Origin', origin);
+        }
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Credentials", true); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header("Access-Control-Allow-Methods", "PUT, DELETE");
